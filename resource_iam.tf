394aadaf257acd9721cc29a4a7e3ee86a49cb822
minjman2659 <minjman2659@gmail.com>
2025-08-19 00:59:58 +0900

    add group policy

--- resource_iam.tf
+++ resource_iam.tf
@@ -28,6 +28,28 @@ resource "aws_iam_group" "devops_group" {
     name = "devops"
 }
 
+resource "aws_iam_group_policy" "devops_group_policy" {
+    name = "devops_group_policy"
+    group = aws_iam_group.devops_group.name
+
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "*"
+            ],
+            "Resource": [
+                "*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_group_membership" "devops" {
     name = aws_iam_group.devops_group.name
 
